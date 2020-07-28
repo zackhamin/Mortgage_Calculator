fe77895dbfeb07d36080b539602b3957f62a1be8
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-28 15:15:50 +0100

    Added while loop and control flow

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -7,12 +7,16 @@ public class helloWorld {
     public static void main(String[] args) {
         final byte PERCENT = 100;
         final byte MONTH_IN_YEARS = 12;
-
-
+        int principal = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
 
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal >= 1_000_000)
+                break;
+            System.out.println("Please enter a value between 1000 and 1000000");
+        }
 
         System.out.print("Annual Interest Rate: ");
         Double interestRate = scanner.nextDouble();
@@ -25,12 +29,6 @@ public class helloWorld {
 
 
         double mortgage = principal * (calculateInterest * Math.pow(1 + calculateInterest, numberOfPayments)) / (Math.pow(1 + calculateInterest, numberOfPayments) -1);
-
-        System.out.println(numberOfPayments);
-        System.out.println(calculateInterest);
-
-        Currency mortgageFormatted = NumberFormat.getCurrencyInstance().getCurrency();
-        Currency currencyResult = mortgageFormatted;
         System.out.println("Your repayment is £" + mortgage);
 
 
