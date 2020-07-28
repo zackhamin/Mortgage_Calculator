caf3b40883ec051b5e82a0c4ca589ca476ef5234
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-28 15:25:04 +0100

    while loops added

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,3 +1,4 @@
+import java.beans.beancontext.BeanContextServiceRevokedEvent;
 import java.text.NumberFormat;
 import java.util.Currency;
 import java.util.Scanner;
@@ -8,29 +9,40 @@ public class helloWorld {
         final byte PERCENT = 100;
         final byte MONTH_IN_YEARS = 12;
         int principal = 0;
+        double interestRate = 0.0;
+        double calculateInterest = 0.0;
+        int numberOfPayments = 0;
+        int periodInYears = 0;
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("Principal: ");
             principal = scanner.nextInt();
-            if (principal >= 1000 && principal >= 1_000_000)
+            if (principal >= 1000 && principal <= 1_000_000)
                 break;
             System.out.println("Please enter a value between 1000 and 1000000");
         }
 
-        System.out.print("Annual Interest Rate: ");
-        Double interestRate = scanner.nextDouble();
-        double calculateInterest = (double)((interestRate / PERCENT) / MONTH_IN_YEARS);
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            interestRate = scanner.nextDouble();
+            if (interestRate > 0 && interestRate <= 30)
+                break;
+            System.out.println("Please enter a value between 1 and 30");
+        }
+        calculateInterest = (double) ((interestRate / PERCENT) / MONTH_IN_YEARS);
 
-        System.out.print("Period in years: ");
-        int periodInYears = scanner.nextInt();
-        int numberOfPayments = periodInYears * MONTH_IN_YEARS;
+        while (true) {
+            System.out.print("Period in years: ");
+            periodInYears = scanner.nextInt();
+            if (periodInYears >= 5 && periodInYears <= 45)
+                break;
+            System.out.println("Please pick a year range between 5 and 45");
+        }
 
-
-
+        numberOfPayments = periodInYears * MONTH_IN_YEARS;
         double mortgage = principal * (calculateInterest * Math.pow(1 + calculateInterest, numberOfPayments)) / (Math.pow(1 + calculateInterest, numberOfPayments) -1);
         System.out.println("Your repayment is £" + mortgage);
-
-
     }
 }
