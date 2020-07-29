240f7ceacc745b0a42703df722a279da78b6792d
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-29 11:25:24 +0100

    refactor code from 90 lines to 45

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -11,34 +11,25 @@ public class helloWorld {
         float interestRate = 0;
         byte periodInYears = 0;
 
-        Scanner scanner = new Scanner(System.in);
 
+        readNumber("Principal ", 1000, 1_000_000);
+        readNumber("Annual Interest Rate: ", 1, 30);
+        readNumber("Period in years: ", 5, 45);
 
-
-        while (true) {
-            System.out.print("Principal: ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Please enter a value between 1000 and 1,000,000");
-        }
-
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            interestRate = scanner.nextFloat();
-            if (interestRate > 0 && interestRate <= 30)
-                break;
-            System.out.println("Please enter a value between 1 and 30");
-        }
-
-        while (true) {
-            System.out.print("Period in years: ");
-            periodInYears = scanner.nextByte();
-            if (periodInYears >= 5 && periodInYears <= 45)
-                break;
-            System.out.println("Please pick a year range between 5 and 45");
-        }
         calculateMortgage(principal, interestRate, periodInYears);
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value > min && value <= max)
+                break;
+            System.out.println("Please enter a value between " + min + " and " + max);
+        }
+        return value;
     }
 
     public static double calculateMortgage(int principal, float interestRate, byte periodInYears){
